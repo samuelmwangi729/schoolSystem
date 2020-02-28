bbd1ed212eb96fba35cde0b18c77bf6a31d8788a
samuel mwangi <samuelmwangi729@gmail.com>
2020-02-28 17:30:27 +0300

    final

---
+++ include/footer.php
@@ -0,0 +1,11 @@
+<footer>
+				<div class="footer-inner">
+					<div class="pull-left">
+					  &copy;<a href="tandc.php"><span style="color:red">Elite</span><span style="color:blue">SecurityConsultants</span></a>&nbsp;<?php echo date('Y');?> All Rights Reserved
+					</div>
+					<div class="pull-right">
+						<button class="badge badge-danger" style="text-align: right;margin-top:15px">eSUnisol V1.0</button>
+						<span class="go-top"><i class="ti-angle-up"></i></span>
+					</div>
+				</div>
+			</footer>
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Please do not change anything in the database
